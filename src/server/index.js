a36a57cf9c76b594be6b4f42b3ea3d1e566e2685
ikicha <ikicha90@gmail.com>
2017-03-05 16:44:27 +0900

    react init

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
+
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.use('/', express.static('../public'));
+app.use(express.static(__dirname + '/../public'));
 io.on('connection', () => console.log("connection"));
 server.listen(3000);
